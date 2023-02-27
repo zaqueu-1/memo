b2e4269395bdd1ab3eba3a00d04a72399002e844
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-02-27 18:09:32 -0300

    fix: vercel fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProvider } from './Contexts/appContext';
-import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
